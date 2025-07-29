a23ae8fcba4c64d6a8821a2293c1fdea58e4362c
realviirless <screep04@gmail.com>
2025-07-29 22:30:02 +0200

    Update 1.4

--- src/main/java/org/viirless/smartban/BanCommand.java
+++ src/main/java/org/viirless/smartban/BanCommand.java
@@ -9,7 +9,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import java.util.Date;
 import java.util.Set;
-import org.bukkit.configuration.ConfigurationSection;
 
 public class BanCommand implements CommandExecutor {
 
@@ -29,57 +28,50 @@ public class BanCommand implements CommandExecutor {
 
         // Check arguments
         if (args.length == 0) {
-            showBanUsage(sender);
+            // Show available ban IDs, reasons, and durations
+            sender.sendMessage(colorize("&6Available Ban IDs:"));
+            sender.sendMessage(colorize("&7Usage: &e/ban <player> <id>"));
+            sender.sendMessage("");
+
+            if (plugin.getConfig().getConfigurationSection("bans") != null) {
+                for (String id : plugin.getConfig().getConfigurationSection("bans").getKeys(false)) {
+                    String reason = plugin.getConfig().getString("bans." + id + ".reason");
+                    String duration = plugin.getConfig().getString("bans." + id + ".duration");
+                    sender.sendMessage(colorize("&eID " + id + ": &f" + reason + " &7(" + duration + ")"));
+                }
+            }
             return true;
         }
 
-        if (args.length < 2) {
+        if (args.length != 2) {
             sender.sendMessage(colorize(plugin.getConfig().getString("messages.usage-ban")));
             return true;
         }
 
         String playerName = args[0];
-        boolean useIdSystem = plugin.getConfig().getBoolean("settings.use-id-system.ban", true);
+        String banIdStr = args[1];
 
-        // Combine all arguments after player name for the reason/id
-        StringBuilder inputBuilder = new StringBuilder();
-        for (int i = 1; i < args.length; i++) {
-            inputBuilder.append(args[i]);
-            if (i < args.length - 1) {
-                inputBuilder.append(" ");
-            }
+        // Validate ban ID
+        int banId;
+        try {
+            banId = Integer.parseInt(banIdStr);
+        } catch (NumberFormatException e) {
+            Set<String> availableIds = plugin.getConfig().getConfigurationSection("bans").getKeys(false);
+            String idsString = String.join(", ", availableIds);
+            String message = plugin.getConfig().getString("messages.invalid-ban-id")
+                    .replace("{ids}", idsString);
+            sender.sendMessage(colorize(message));
+            return true;
         }
-        String input = inputBuilder.toString();
 
-        // Find the ban entry either by ID or reason
-        String banId = null;
-        ConfigurationSection bans = plugin.getConfig().getConfigurationSection("bans");
-
-        if (useIdSystem) {
-            // Using ID system
-            if (!bans.contains(input)) {
-                Set<String> availableIds = bans.getKeys(false);
-                String idsString = String.join(", ", availableIds);
-                String message = plugin.getConfig().getString("messages.invalid-ban-id")
-                        .replace("{ids}", idsString);
-                sender.sendMessage(colorize(message));
-                return true;
-            }
-            banId = input;
-        } else {
-            // Using reason system - find the ID by reason
-            for (String id : bans.getKeys(false)) {
-                String reason = bans.getString(id + ".reason");
-                if (reason != null && reason.equalsIgnoreCase(input)) {
-                    banId = id;
-                    break;
-                }
-            }
-
-            if (banId == null) {
-                sender.sendMessage(colorize("&cInvalid ban reason! Use TAB to see available reasons."));
-                return true;
-            }
+        // Check if ban ID exists in config
+        if (!plugin.getConfig().contains("bans." + banId)) {
+            Set<String> availableIds = plugin.getConfig().getConfigurationSection("bans").getKeys(false);
+            String idsString = String.join(", ", availableIds);
+            String message = plugin.getConfig().getString("messages.invalid-ban-id")
+                    .replace("{ids}", idsString);
+            sender.sendMessage(colorize(message));
+            return true;
         }
 
         // Get player (online or offline) - allows pre-emptive bans
@@ -134,42 +126,6 @@ public class BanCommand implements CommandExecutor {
         sender.sendMessage(colorize(successMessage));
 
         return true;
-    }
-
-    private void showBanUsage(CommandSender sender) {
-        String divider = plugin.getConfig().getString("usage-format.divider", "&7&m--------------------------------");
-        boolean useIdSystem = plugin.getConfig().getBoolean("settings.use-id-system.ban", true);
-
-        String header = useIdSystem ?
-            plugin.getConfig().getString("usage-format.ban-command.header", "&cUsage: &7/ban <player> <ban-id>") :
-            "&cUsage: &7/ban <player> <reason>";
-
-        String listHeader = useIdSystem ?
-            plugin.getConfig().getString("usage-format.ban-command.list-header", "&cAvailable Ban IDs:") :
-            "&cAvailable Ban Reasons:";
-
-        String format = useIdSystem ?
-            plugin.getConfig().getString("usage-format.ban-command.format", "&7ID: &c{id} &7| Reason: &c{reason} &7| Duration: &c{duration}") :
-            "&7Reason: &c{reason} &7| Duration: &c{duration}";
-
-        sender.sendMessage(colorize(divider));
-        sender.sendMessage(colorize(header));
-        sender.sendMessage(colorize(divider));
-        sender.sendMessage(colorize(listHeader));
-
-        if (plugin.getConfig().getConfigurationSection("bans") != null) {
-            for (String id : plugin.getConfig().getConfigurationSection("bans").getKeys(false)) {
-                String reason = plugin.getConfig().getString("bans." + id + ".reason");
-                String duration = plugin.getConfig().getString("bans." + id + ".duration");
-                String line = format
-                    .replace("{id}", id)
-                    .replace("{reason}", reason)
-                    .replace("{duration}", duration);
-                sender.sendMessage(colorize(line));
-            }
-        }
-
-        sender.sendMessage(colorize(divider));
     }
 
     private long parseDuration(String duration) {
--- src/main/java/org/viirless/smartban/BanPlugin.java
+++ src/main/java/org/viirless/smartban/BanPlugin.java
@@ -19,23 +19,12 @@ public class BanPlugin extends JavaPlugin {
         // Create bans.yml file for storing active bans
         createBansFile();
 
-        // Create TabCompleter instance
-        BanTabCompleter tabCompleter = new BanTabCompleter(this);
-
-        // Register commands and tab completers
+        // Register commands
         getCommand("ban").setExecutor(new BanCommand(this));
         getCommand("unban").setExecutor(new UnbanCommand(this));
-        getCommand("kick").setExecutor(new KickCommand(this));
-        getCommand("mute").setExecutor(new MuteCommand(this));
-        getCommand("unmute").setExecutor(new UnmuteCommand(this));
 
-        // Register tab completers
-        getCommand("ban").setTabCompleter(tabCompleter);
-        getCommand("mute").setTabCompleter(tabCompleter);
-
-        // Register event listeners
+        // Register event listener
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
-        getServer().getPluginManager().registerEvents(new ChatListener(this), this);
 
         getLogger().info("Ban Plugin has been enabled!");
     }
